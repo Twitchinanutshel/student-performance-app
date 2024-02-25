3b13298d43ca6e1bc320f4dd664c36de2bec860a
Noah Gauci <breakf117@gmail.com>
2024-02-25 19:14:41 +1100

    13th

--- main.js
+++ main.js
@@ -11,9 +11,9 @@ addBtn.addEventListener('click', function(){
     newClassCard.classList.add('class-card');
     const className = document.createElement('p');
 
-    if (cardCounter < 5){
+    if (cardCounter < 30){
         cardCounter += 1
-    } else if (cardCounter >= 5){
+    } else if (cardCounter >= 30){
         newClassCard = null
     }
 
@@ -39,15 +39,28 @@ addBtn.addEventListener('click', function(){
     icon.classList.add("fa-solid", "fa-school", "fa-5x");
     icon.style.color = "#6054b8";
 
+    // create icon for removing button
+    const removeIcon = document.createElement('button');
+    removeIcon.classList.add("fa-solid", "fa-xmark", "fa-2x", 'xmark');
+    removeIcon.style.color = "black";
+    removeIcon.style.display = 'none'
+
+    removeIcon.addEventListener('click', function(){
+        mainPage.removeChild(newClassCard)
+
+        cardCounter -= 1
+    })
+
+
 
     // make it so user can add text to class name
-
     className.contentEditable = true;
     className.textContent = "New Class"; // Default name
     className.classList.add("editable");
 
     // add the school class icon
-    // and add the users text that was inputed 
+    // and add the users text that was inputed
+    newClassCard.appendChild(removeIcon);
     newClassCard.appendChild(icon);
     newClassCard.appendChild(className);
     mainPage.appendChild(newClassCard);
@@ -63,6 +76,19 @@ addBtn.addEventListener('click', function(){
 removeBtn.addEventListener('click', function(){
     const classCards = document.querySelectorAll('.class-card');
     classCards.forEach((card) => {
+        const closeIcon = card.querySelector('.xmark');
         card.classList.add('removeAnimation');
-    })
+        closeIcon.style.display = 'block';
+
+        document.addEventListener('click', function(event) {
+            const target = event.target;
+            if (!target.classList.contains('xmark') && target !== removeBtn) {
+                classCards.forEach((card) => {
+                    const closeIcon = card.querySelector('.xmark');
+                    card.classList.remove('removeAnimation');
+                    closeIcon.style.display = 'none';
+                });
+            }
+        });
+    });
 });
